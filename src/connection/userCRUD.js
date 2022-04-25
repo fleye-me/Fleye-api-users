47322681db11432d91a7c22395568a4e100e7f62
Julia Godoy <leuzegodoyjulia@gmail.com>
2022-04-25 14:49:21 -0300

    refactor: environment variables

--- src/connection/userCRUD.js
+++ src/connection/userCRUD.js
@@ -3,14 +3,15 @@
 */
 
 const User = require('../schemas/user.schema');
+require('dotenv').config();
 
 const Pool = require('pg').Pool;
 const pool = new Pool({
-  user: 'admin',
-  host: 'localhost',
-  database: 'apiusuarios',
-  password: 'admin',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.PORT,
 });
 
 const { initQuery, adjustError } = require('../common/query-helper');
